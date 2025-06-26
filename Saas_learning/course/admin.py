3c37b272e55c12d9b504b57bfb2413de5229d220
SAHIL15KP <sahilkumar1582004@gmail.com>
2025-06-26 05:37:17 +0530

    templates added

--- Saas_learning/course/admin.py
+++ Saas_learning/course/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course 
+@admin.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title","instructor", "created_at", "updated_at"]
+    search_fields =  ["title","instructor__username"]
+    list_filter = ["created_at", "updated_at", "instructor"]
